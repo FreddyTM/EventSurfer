5b2b3cfa1cd867fed3b630d56081fc96e68303fa
Freddy <atm64ioc@gmail.com>
2020-11-01 19:48:20 +0100

    Se añade conexión jdbc a la base de datos surferdb en la clase appDummy

--- src/app/AppDummy.java
+++ src/app/AppDummy.java
@@ -1,13 +1,47 @@
 
 package app;
 
-//VERSION 0.0.1
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//VERSION 0.0.2
+//Se añade conexión a la base de datos
 
 public class AppDummy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AppDummy appDummy = new AppDummy();
+		appDummy.go();
 	}
+	
+	public void go() {
+		connectToDatabase();
+	}
+	
+	public void connectToDatabase () {
+		Connection connection = null;
+		String url = "jdbc:postgresql://localhost:5432/surferdb";
+		String user = "surferadmin";
+		String password = "surferpass";
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			} catch (ClassNotFoundException ex) {
+				System.out.println("No se encuentra el controlador JDBC ("
+				+ ex.getMessage() +")");
+			}
+		
+		try{
+			//Obtenim una connexió des de DriverManager
+			connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Connexió realitzada usant"
+			+ " DriverManager");
+			connection.close();
+			 
+			} catch (SQLException ex) {
+			System.out.println("Error " + ex.getMessage());
+			}
+		}
 
 }
