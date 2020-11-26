b6234406c0bc9ba674a9c5678cfd13bb69d439ad
Freddy <atm64ioc@gmail.com>
2020-11-26 10:43:54 +0100

    Update Area Dev 0.0.19

--- src/company/Area.java
+++ src/company/Area.java
@@ -170,6 +170,22 @@ public class Area {
 	 * @param id id del area buscado
 	 * @return area con el id entrado por parámetro (null si no existe)
 	 */
+	public Area getAreaByName (BusinessUnit bUnit, String name) {
+		List<Area> areaList = bUnit.getAreas();
+		for (Area area: areaList) {
+			if (area.getArea() == name) {
+				return area;
+			}
+		}	
+		return null;
+	}
+	
+	/**
+	 * Devuelve el area al que pertenece el id entrado por parámetro
+	 * @param bUnit BusinessUnit del que comprobamos la lista de areas
+	 * @param id id del area buscado
+	 * @return area con el id entrado por parámetro (null si no existe)
+	 */
 	public Area getAreaById (BusinessUnit bUnit, int id) {
 		List<Area> areaList = bUnit.getAreas();
 		for (Area area: areaList) {
