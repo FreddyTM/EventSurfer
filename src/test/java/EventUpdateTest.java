e5c1c72475547db2473fac851d31ae6f5a42c868
FreddyGL <atm64ioc@gmail.com>
2020-12-08 19:08:30 +0100

    Add EventTest, EventUpdateTest ver 0.0.22

---
+++ src/test/java/EventUpdateTest.java
@@ -0,0 +1,54 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EventUpdateTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSaveEventUpdateToDB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddNewEventUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateEventUpdateToDB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEventUpdatesFromDB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEventUpdatesByUserId() {
+		fail("Not yet implemented");
+	}
+
+}
