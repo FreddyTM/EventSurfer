a992ceaa825f9b8015c094cb8803be4d850670b4
Freddy <atm64ioc@gmail.com>
2021-01-26 11:57:19 +0100

    Add UserUI ver 0.1.6

---
+++ src/main/java/gui/UserUI.java
@@ -0,0 +1,28 @@
+package main.java.gui;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class UserUI extends JPanel {
+
+	public UserUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserUI(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserUI(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserUI(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
