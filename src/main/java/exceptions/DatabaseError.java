a2e22224f7fc35e5b37f6e397eab32cc7b994604
Freddy <atm64ioc@gmail.com>
2021-10-19 12:59:18 +0200

    Update DatabaseError delete comments ver 0.1.11

--- src/main/java/exceptions/DatabaseError.java
+++ src/main/java/exceptions/DatabaseError.java
@@ -8,7 +8,7 @@ public class DatabaseError extends Exception {
 	public static final String TITLE = "Database error";
 	
 	public DatabaseError() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DatabaseError(String message) {
@@ -23,17 +23,17 @@ public class DatabaseError extends Exception {
 
 	public DatabaseError(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DatabaseError(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public DatabaseError(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
