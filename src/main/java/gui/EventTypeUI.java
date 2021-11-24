d79765fface55c3e1e6e698c15579c006b4716c7
Freddy <atm64ioc@gmail.com>
2021-11-24 19:47:26 +0100

    Update EventTypeUI add buttons & actions ver 0.1.12

--- src/main/java/gui/EventTypeUI.java
+++ src/main/java/gui/EventTypeUI.java
@@ -1,16 +1,23 @@
 package main.java.gui;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Timer;
 
+import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 import main.java.session.CurrentSession;
 import main.java.toolbox.ToolBox;
@@ -35,7 +42,7 @@ public class EventTypeUI extends JPanel {
 	//Registra si el panel está visible o no
 	private boolean panelVisible;
 	
-	private JTextField EventTypeNameField = new JTextField();
+//	private JTextField EventTypeNameField = new JTextField();
 	//Lista de todas las areas existentes en la base de datos
 	private List<EventType> allEventTypes;
 	//Registra el area seleccionada en cada momento
@@ -46,7 +53,13 @@ public class EventTypeUI extends JPanel {
 	private String textFieldBackup;
 	
 	//Elementos gráficos
-	
+	private JTextField eventTypeNameField = new JTextField();
+	private JLabel infoLabel;
+	private JButton editButton = new JButton();
+	private JButton cancelButton;
+	private JButton oKButton;
+	private JButton newButton;
+	private JButton deleteButton = new JButton();
 	
 	//Elementos que aparecerán en la lista de tipos de eventos
 	private String[] registeredEventTypes;
@@ -57,32 +70,146 @@ public class EventTypeUI extends JPanel {
 	//Contenedor de la lista de tipos de evento
 	private JScrollPane registeredScrollPane;
 	
-//	private final Action editAction = new EditAction();
-//	private final Action cancelAction = new CancelAction();
-//	private final Action oKAction = new OKAction();
-//	private final Action newAction = new NewAction();
-//	private final Action deleteAction = new DeleteAction();
+	private final Action editAction = new EditAction();
+	private final Action cancelAction = new CancelAction();
+	private final Action oKAction = new OKAction();
+	private final Action newAction = new NewAction();
+	private final Action deleteAction = new DeleteAction();
 	
 	//Registra la acción a realizar por el botón aceptar
 	private int okActionSelector = EventTypeUI.OK_ACTION_UNDEFINED;
 	
 	public EventTypeUI(CurrentSession session) {
-		// TODO Auto-generated constructor stub
+		this.session = session;
+		setLayout(null);
+		panelVisible = true;
+		
+		JTextPane eventTypeTxt = new JTextPane();
+		eventTypeTxt.setText("GESTIÓN DE TIPOS DE EVENTO");
+		eventTypeTxt.setFont(new Font("Tahoma", Font.BOLD, 20));
+		eventTypeTxt.setFocusable(false);
+		eventTypeTxt.setEditable(false);
+		eventTypeTxt.setBackground(UIManager.getColor(this.getBackground()));
+		eventTypeTxt.setBounds(50, 50, 380, 30);
+		add(eventTypeTxt);
+		
+		JLabel eventTypeLabel = new JLabel("Tipo de evento");
+		eventTypeLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		eventTypeLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		eventTypeLabel.setBounds(50, 125, 200, 25);
+		add(eventTypeLabel);
+		
+		eventTypeNameField.setBackground(UIManager.getColor(new JPanel().getBackground()));
+		eventTypeNameField.setBounds(260, 125, 400, 25);
+		eventTypeNameField.setEditable(false);
+		add(eventTypeNameField);
+		
+		infoLabel = new JLabel();
+		infoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		infoLabel.setBounds(50, 175, 900, 25);
+		add(infoLabel);
+		
+		newButton = new JButton();
+		newButton.setAction(newAction);
+		newButton.setBounds(229, 225, 89, 23);
+		if (session.getUser().getUserType().equals("USER")) {
+			newButton.setEnabled(false);
+		}
+		add(newButton);
+		
+		editButton.setAction(editAction);
+		editButton.setBounds(329, 225, 89, 23);
+		if (session.getUser().getUserType().equals("USER")
+				|| selectedEventType == null) {
+			editButton.setEnabled(false);
+		}
+		add(editButton);
+		
+		deleteButton.setAction(deleteAction);
+		deleteButton.setBounds(429, 225, 89, 23);
+		if (session.getUser().getUserType().equals("USER")
+				|| selectedEventType == null) {
+			deleteButton.setEnabled(false);
+		}
+		add(deleteButton);
+		
+		cancelButton = new JButton();
+		cancelButton.setAction(cancelAction);
+		cancelButton.setBounds(529, 225, 89, 23);
+		cancelButton.setEnabled(false);
+		add(cancelButton);
+		
+		oKButton = new JButton();
+		oKButton.setAction(oKAction);
+		oKButton.setBounds(629, 225, 89, 23);
+		oKButton.setEnabled(false);
+		add(oKButton);
 	}
-
-//	public EventTypeUI(LayoutManager layout) {
-//		super(layout);
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	public EventTypeUI(boolean isDoubleBuffered) {
-//		super(isDoubleBuffered);
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	public EventTypeUI(LayoutManager layout, boolean isDoubleBuffered) {
-//		super(layout, isDoubleBuffered);
-//		// TODO Auto-generated constructor stub
-//	}
-
+	
+	
+	
+	public class NewAction extends AbstractAction {
+		public NewAction() {
+			putValue(NAME, "Nuevo");
+			putValue(SHORT_DESCRIPTION, "Add new area");
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public class EditAction extends AbstractAction {
+		public EditAction() {
+			putValue(NAME, "Editar");
+			putValue(SHORT_DESCRIPTION, "Enable data edit");
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public class CancelAction extends AbstractAction {
+		public CancelAction() {
+			putValue(NAME, "Cancelar");
+			putValue(SHORT_DESCRIPTION, "Cancel data edit");
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public class DeleteAction extends AbstractAction {
+		public DeleteAction() {
+			putValue(NAME, "Borrar");
+			putValue(SHORT_DESCRIPTION, "Delete data");
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public class OKAction extends AbstractAction {
+		public OKAction() {
+			putValue(NAME, "Aceptar");
+			putValue(SHORT_DESCRIPTION, "Save new data or data edit");
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
