04246e03bf0ff847e15bd23e75bf07daf646966d
FreddyGL <atm64ioc@gmail.com>
2021-12-23 17:27:04 +0100

    Update EventDataUI add listeners & refresh comboBox ver 0.1.14

--- src/main/java/gui/EventDataUI.java
+++ src/main/java/gui/EventDataUI.java
@@ -2,11 +2,14 @@ package main.java.gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -17,6 +20,7 @@ import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 
 import main.java.company.BusinessUnit;
+import main.java.company.Company;
 import main.java.session.CurrentSession;
 import main.java.toolbox.ToolBox;
 
@@ -128,6 +132,87 @@ public class EventDataUI extends JPanel{
 			}
 		}
 		return 0;
+	}
+	
+	/**
+	 * Actualiza el contenido del comboBox que selecciona la unidad de negocio activa
+	 */
+	private void refreshComboBox() {
+		comboList = getComboBoxItemsFromSession(activeFilterCheckBox.isSelected());
+		comboBox.setModel(new DefaultComboBoxModel(comboList));
+		comboBox.setSelectedIndex(getSelectedIndexFromArray(comboList));
+	}
+	
+	/**
+	 * Listener que define el comportamiento del objeto comboBox. Cada elemento se corresponde con
+	 * las unidades de negocio de la compañía que se han cargado en la sesión. Por el nombre seleccionado
+	 * se localiza el objeto BusinessUnit al que pertenece y se asigna dicho objeto como unidad de negocio
+	 * de la sessión, reemplazando al que hubiera hasta ese momento. Si activeFilterCheckBox está seleccionado,
+	 * no se mostrarán las unidades de negocio que estén marcadas como no activas
+	 */
+	private class ComboListener implements ItemListener {
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			String item = (String) comboBox.getSelectedItem();
+			Company company = session.getCompany();
+			//Recuperamos la unidad de negocio seleccionada
+			BusinessUnit selectedBunit = new BusinessUnit().getBusinessUnitByName(company, item);			
+			//La asignamos a la sesión
+			session.setbUnit(selectedBunit);
+//			//Mostramos sus datos
+//			populateTextFields();
+//			//Hacemos backup del contenido de los datos del formulario
+//			updateDataCache();
+			
+			//EN PRINCIPIO NO REACTIVAR
+//			//Vaciamos label de información
+//			infoLabel.setText("");
+		}
+		
+	}
+	
+	/**
+	 * Listener que define el comportamiento del checkbox activeFilterCheckBox.
+	 * Si activamos el checkbox y la unidad de negocio en pantalla está activa no habrá ningún cambio. Si no está activa,
+	 * la unidad de negocio de la sesión pasará a ser la del usuario que abrió sesión, y se mostrará en pantalla. En ese
+	 * caso comboBox dejará de mostrar las unidades de negocio no activas.
+	 * Si desactivamos el checkbox no habrá ningún cambio. comboBox pasará a mostrar todas las unidades de negocio cargadas en
+	 * la sesión.
+	 */
+	private class CheckBoxListener implements ItemListener {
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			
+			int state = e.getStateChange();
+			if (state == ItemEvent.SELECTED) {
+				//Si la bUnit de la sesión no está activa
+				if (session.getbUnit().isActivo() == false) {		
+					//Buscamos la bUnit del usuario que abrió sesión
+					BusinessUnit userBunit = new BusinessUnit().getBusinessUnitById(session.getCompany(), session.getUser().getbUnit().getId());
+					//La asignamos como bUnit de la sesión
+					session.setbUnit(userBunit);
+					//Mostramos sus datos
+//					populateTextFields();
+//					//Hacemos backup del contenido de los datos del formulario
+//					updateDataCache();
+					
+					//Renovamos la lista de las unidades de negocio del comboBox
+					refreshComboBox();
+					
+//					//Vaciamos label de información
+//					infoLabel.setText("");
+				//Si la bUnit de la sesión está activa, hay que renovar el combobox igualmente para que ya no salgan las bUnits no activas
+				} else {
+					//Renovamos la lista de las unidades de negocio del comboBox
+					refreshComboBox();
+				}
+			} else if (state == ItemEvent.DESELECTED) {
+				//Renovamos la lista de las unidades de negocio del comboBox
+				refreshComboBox();
+			}
+		}
 	}
 
 }
